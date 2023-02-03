e65646102a0949698cc86a3bf60582194b13c7c0
Jatin Kumar <main.jatink@gmail.com>
2023-02-03 18:16:03 +0530

    first commit

---
+++ evaluationTask/TabView.swift
@@ -0,0 +1,43 @@
+import SwiftUI
+
+struct MainTabView: View {
+    @State private var selectedRowIndex = 0
+    
+    var body: some View {
+        TabView(selection: $selectedRowIndex) {
+            VStack {
+                Image("dp_formal")
+                    .resizable()
+                    .frame(width: 300, height: 300)
+                
+                Text("Hi, this is Jatin Kumar.\nI'm a App Dev member in Swift Coding CLub")
+                    .frame(width: 320)
+                
+            }
+                .onAppear {
+                    selectedRowIndex = 0
+                }
+                .tabItem {
+                    Label("Previous", systemImage: "backward")
+                }
+                .tag(0)
+            
+            VStack {
+                Text("Next")
+            }
+                .onAppear {
+                    selectedRowIndex = 1
+                }
+                .tabItem {
+                    Label("Previous", systemImage: "forward")
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
---
+++ evaluationTask/evaluationTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  evaluationTaskApp.swift
+//  evaluationTask
+//
+//  Created by Jatin Kumar on 03/02/23.
+//
+
+import SwiftUI
+
+@main
+struct evaluationTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
